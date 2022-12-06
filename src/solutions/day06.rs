5b9744929f5279cffcff700e0c33b74c70baad35
Fraser Li <mail.fraserli@gmail.com>
2022-12-06 22:15:47 +1100

    Update day 6 solution

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -5,24 +5,36 @@ pub fn process_input(input: &str) -> Vec<char> {
 }
 
 pub fn part1(data: &[char]) -> usize {
+    let mut set: HashSet<&char> = HashSet::from_iter(data[0..4].iter());
+
     for i in 0..data.len() - 4 {
-        let buf = &data[i..i + 4];
-        let set: HashSet<&char> = HashSet::from_iter(buf.iter());
         if set.len() == 4 {
             return i + 4;
         }
+
+        if !data[i + 1..i + 4].contains(&data[i]) {
+            set.remove(&data[i]);
+        }
+
+        set.insert(&data[i + 4]);
     }
 
     panic!();
 }
 
 pub fn part2(data: &[char]) -> usize {
+    let mut set: HashSet<&char> = HashSet::from_iter(data[0..14].iter());
+
     for i in 0..data.len() - 14 {
-        let buf = &data[i..i + 14];
-        let set: HashSet<&char> = HashSet::from_iter(buf.iter());
         if set.len() == 14 {
             return i + 14;
         }
+
+        if !data[i + 1..i + 14].contains(&data[i]) {
+            set.remove(&data[i]);
+        }
+
+        set.insert(&data[i + 14]);
     }
 
     panic!();
